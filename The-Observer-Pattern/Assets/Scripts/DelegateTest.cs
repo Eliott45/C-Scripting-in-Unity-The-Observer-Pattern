31465c4e7882fff59cdb3424fee71e9c554f61e9
Maksim <eliott_nu@mail.ru>
2021-09-14 16:18:39 +0900

    Delegate test

---
+++ The-Observer-Pattern/Assets/Scripts/DelegateTest.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelegateTest : MonoBehaviour
+{
+    private delegate void Print(string val);
+    private Print _p;
+
+    private void Awake()
+    { 
+        _p = PrintValue;
+        _p += PrintData;
+    }
+
+    private void Start()
+    {
+        _p("Delegate");
+    }
+
+    private static void PrintData(string s)
+    {
+        Debug.Log("PrintData " + s);
+    }
+
+    private static void PrintValue(string s)
+    {
+        Debug.Log("PrintValue " + s);
+    }
+}
